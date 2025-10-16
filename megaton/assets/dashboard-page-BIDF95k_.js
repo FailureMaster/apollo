4ddcb9ca9bef81f7f2a36760b03a1fe4c034ba00
christianecleviaverzosa-stack <christianecleviaverzosa-stack@users.noreply.github.com>
2025-10-16 14:59:02 +0000

    deploy: c97b62dd4f7afc557dbf60c2f6c3720bc2d13ec8

--- megaton/assets/dashboard-page-BIDF95k_.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-CvpwmExl.js";function t(){return a.jsx("section",{"data-testid":"dashboard-page",children:a.jsx("p",{children:"this is the dashboard page"})})}export{t as default};
---
+++ megaton/assets/dashboard-page-BvJO6B_N.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-DjrWsqgY.js";function t(){return a.jsx("section",{"data-testid":"dashboard-page",children:a.jsx("p",{children:"this is the dashboard page"})})}export{t as default};
